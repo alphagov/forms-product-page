589412ef93fd7658405bcde4443e6971a5f2905b
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2023-06-22 12:01:24 +0100

    Add kill server in tests

--- jest-puppeteer.config.mjs
+++ jest-puppeteer.config.mjs
@@ -7,6 +7,7 @@ export default {
     command: 'bundle exec middleman --port 8888 --watcher-disable',
     port: 8888,
     launchTimeout: 9000,
-    debug: true
+    debug: true,
+    usedPortAction: 'kill'
   }
 }
