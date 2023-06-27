2891d77ea57e4c1806667d2e22563b12f12adeaa
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2023-06-27 13:38:11 +0100

    Add google tag activation code

--- source/javascripts/site.mjs
+++ source/javascripts/site.mjs
@@ -1,3 +1,22 @@
 import { initAll } from 'govuk-frontend'
 
+import { loadConsentStatus, CONSENT_STATUS } from './services/cookie.mjs'
+import { installAnalyticsScript, setDefaultConsent, sendPageViewEvent, attachExternaLinkTracker } from './services/google_tag.mjs'
+
 initAll()
+
+const analyticsConsentStatus = loadConsentStatus()
+
+setDefaultConsent(analyticsConsentStatus === CONSENT_STATUS.GRANTED)
+
+// send pageview regardless of consent value - if consent has not been granted
+// yet, GTM won't be loaded and no data is sent to Google analytics. Doing this
+// now means that if consent is granted later on this page, the event will be
+// sent
+sendPageViewEvent()
+attachExternaLinkTracker()
+
+if (analyticsConsentStatus === CONSENT_STATUS.GRANTED) {
+  installAnalyticsScript(window)
+}
+
