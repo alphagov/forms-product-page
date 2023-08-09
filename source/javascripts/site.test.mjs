afa5551ad78e0efac3911dc8ff16864ee20ae24f
Jamie Wilkinson <jamie.wilkinson@digital.cabinet-office.gov.uk>
2023-08-09 11:01:28 +0100

    WIP Initial deletion of existing product pages

--- source/javascripts/site.test.mjs
+++
@@ -1,9 +0,0 @@
-beforeAll(async () => {
-  await import('./site.mjs')
-})
-
-// An example test which doesn't do anything other than check that the test
-// setup is working correctly
-test('simple test', () => {
-  expect(true).toBe(true)
-})
