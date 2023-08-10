afb0c9dc54d3dab6b1ab6babe699edf6f4901cea
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2023-08-10 13:22:09 +0100

    Add pages controller, view, route
     Add a controller, view and route for a simple index page.
    
     The pages controller will be used for all the pages within the current
     middleman project.
    
     The next PR will be to add the fontend build and style the example
     content with GOV.UK.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get :ping, controller: :heartbeat
+
+  # Defines the root path route ("/")
+  root "pages#index"
 end
---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe "Pages", type: :request do
+  describe "GET /index" do
+    it "returns ok" do
+      get root_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
