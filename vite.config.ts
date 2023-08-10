086ba3e0343cfab295a85dfef926c0c7675d0fcb
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2023-08-10 13:21:28 +0100

    Add sass and viteconfig
    Install node sass, using npm and configure vite.config.ts to build scss
    files. The configuration matches that in forms-admin and npm is used
    there too.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import * as path from 'node:path'
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 
@@ -5,4 +6,20 @@ export default defineConfig({
   plugins: [
     RubyPlugin(),
   ],
+  build: { emptyOutDir: true },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        includePaths: ['./node_modules/govuk-frontend/'],
+        quietDeps: true
+      },
+      devSourcemaps: true
+    }
+  },
+  resolve: {
+    alias: {
+      '@govuk': path.resolve(__dirname, 'node_modules/govuk-frontend/govuk'),
+      '@images': path.resolve(__dirname, 'app/frontend/images')
+    }
+  }
 })
