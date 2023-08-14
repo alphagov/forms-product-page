8d8f42315c923120d68a15d14b61c5ab09870faa
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2023-08-14 09:23:54 +0100

    Add page_title helper
     Taken from forms-admin.
    
     In forms-admin, we use html_safe on the title argument to allow html to
     be passed in to the helper. That isn't needed here - titles are not
     dynamic and so it's been removed.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def govuk_assets_path
     "/node_modules/govuk-frontend/govuk/assets"
   end
+
+  def page_title(separator = " – ")
+    [content_for(:title), "GOV.UK Forms"].compact.join(separator)
+  end
+
+  def set_page_title(title)
+    content_for(:title) { title }
+  end
 end
