a0e5b5d88434d1c814f4731fdb2c716624e673f8
Jamie Wilkinson <jamie.wilkinson@digital.cabinet-office.gov.uk>
2023-08-14 16:51:56 +0100

    Add custom 404 and 500 error pages
    Add branded error pages for 404 and 500 status codes, with associated
    tests and clean up of default error pages.

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   get "/accessibility" => "pages#accessibility"
   get "/cookies" => "pages#cookies"
   get "/privacy" => "pages#privacy"
+
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 end
---
+++ spec/requests/errors_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe "Errors", type: :request do
+  describe "Page not found" do
+    before do
+      get "/404"
+    end
+
+    it "returns http code 404" do
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "renders the not found template" do
+      expect(response.body).to include("Page not found")
+    end
+  end
+
+  describe "Internal server error" do
+    before do
+      get "/500"
+    end
+
+    it "returns http code 500" do
+      expect(response).to have_http_status(:internal_server_error)
+    end
+
+    it "renders the internal server error template" do
+      expect(response.body).to include("Sorry, there is a problem with the service")
+    end
+  end
+end
