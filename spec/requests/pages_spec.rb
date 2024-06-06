621f52a43ee5aabf6f1e8d08ef6d48867614598d
Alistair Laing <aliuk2012@users.noreply.github.com>
2024-06-06 07:48:42 +0100

    replace homepage functional test
    This test may have been a simple test to make sure puppeteer was setup correctly. To keep the test of the home page title, I've added it to a request spec

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,9 +2,16 @@ require "rails_helper"
 
 RSpec.describe "Pages", type: :request do
   describe "GET /index" do
-    it "returns ok" do
+    before do
       get root_path
+    end
+
+    it "returns ok" do
       expect(response).to have_http_status(:ok)
+    end
+
+    it "has the correct title" do
+      expect(Capybara.string(response.body).title).to eq("Create online forms for GOV.UK – GOV.UK Forms")
     end
   end
 
