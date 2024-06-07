34dcd1093fb44c4338510c85199e6e73c1228ba4
Alistair Laing <aliuk2012@users.noreply.github.com>
2024-06-07 13:24:22 +0100

    create a configuration option to disable js
    In a spec example if you set js:false for a system test then instead of using chrome/headless chrome driver it will revert to capybara default RackTest.
    
    see https://github.com/teamcapybara/capybara#racktest

--- spec/support/capybara_selenium_webdriver.rb
+++ spec/support/capybara_selenium_webdriver.rb
@@ -2,4 +2,8 @@ RSpec.configure do |config|
   config.before(:example, type: :system) do
     driven_by(Settings.show_browser_during_tests ? :selenium : :selenium_chrome_headless)
   end
+
+  config.before(:example, js: false) do
+    Capybara.current_driver = :rack_test
+  end
 end
