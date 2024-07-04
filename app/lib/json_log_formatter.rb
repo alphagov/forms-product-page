f85653be7305f7c1fd72cb6169d644e7bcc8ae51
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2024-07-04 15:54:38 +0100

    Fix lint issues with method names

--- app/lib/json_log_formatter.rb
+++ app/lib/json_log_formatter.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
+
 class JsonLogFormatter < ActiveSupport::Logger::Formatter
   def call(severity, timestamp, _progname, message)
     log_event = {
       level: severity,
-      time: timestamp,
+      time: timestamp
     }
 
-    if is_json(message)
+    if json?(message)
       log_event.merge!(JSON.parse(message))
     else
       log_event[:message] = message
@@ -15,7 +16,7 @@ class JsonLogFormatter < ActiveSupport::Logger::Formatter
     "#{log_event.to_json}\n"
   end
 
-  def is_json(message)
+  def json?(message)
     result = JSON.parse(message)
     result.is_a?(Hash) || result.is_a?(Array)
   rescue JSON::ParserError, TypeError
