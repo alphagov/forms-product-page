159dd9f1adb4c6dbfc5c7ba99e41dbadede2fe0c
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2024-07-04 15:54:08 +0100

    Fix lint issues with setter prefixes
    Methods should not use the `set_` (or `get_`) prefix when they aren't getter or setter methods.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,29 +5,29 @@ module ApplicationHelper
     "/node_modules/govuk-frontend/dist/govuk/assets"
   end
 
-  def page_title(separator = " – ")
-    [content_for(:title), "GOV.UK Forms"].compact.join(separator)
-  end
-
-  def set_page_title(title)
+  def page_title(title)
     content_for(:title) { title }
   end
 
-  def page_description
+  def branded_page_title(separator = " – ")
+    [content_for(:title), "GOV.UK Forms"].compact.join(separator)
+  end
+
+  def page_description(description)
+    content_for(:description) { description.html_safe }
+  end
+
+  def page_description_tag
     description = content_for(:description)
     tag.meta(name: "description", content: description) if description.present?
   end
 
-  def set_page_description(description)
-    content_for(:description) { description.html_safe }
+  def page_robots(directive)
+    content_for(:robots) { directive }
   end
 
-  def page_robots
+  def page_robots_tag
     directive = content_for(:robots)
     tag.meta(name: "robots", content: directive) if directive.present?
-  end
-
-  def set_page_robots(directive)
-    content_for(:robots) { directive }
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -6,17 +6,17 @@ RSpec.describe ApplicationHelper do
   describe "#page_title" do
     it "returns the title with separator and default suffix" do
       helper.content_for(:title, "Test Title")
-      expect(helper.page_title).to eq("Test Title – GOV.UK Forms")
+      expect(helper.branded_page_title).to eq("Test Title – GOV.UK Forms")
     end
 
     it "returns only the default suffix if title is not set" do
-      expect(helper.page_title).to eq("GOV.UK Forms")
+      expect(helper.branded_page_title).to eq("GOV.UK Forms")
     end
   end
 
   describe "#set_page_title" do
     it "sets the page title" do
-      helper.set_page_title("New Title")
+      helper.page_title("New Title")
       expect(helper.content_for(:title)).to eq("New Title")
     end
   end
@@ -24,17 +24,17 @@ RSpec.describe ApplicationHelper do
   describe "#page_description" do
     it "returns the meta tag with description if description is set" do
       helper.content_for(:description, "Test Description")
-      expect(helper.page_description).to eq('<meta name="description" content="Test Description">')
+      expect(helper.page_description_tag).to eq('<meta name="description" content="Test Description">')
     end
 
     it "returns nil if description is not set" do
-      expect(helper.page_description).to be_nil
+      expect(helper.page_description_tag).to be_nil
     end
   end
 
   describe "#set_page_description" do
     it "sets the page description" do
-      helper.set_page_description("New Description")
+      helper.page_description("New Description")
       expect(helper.content_for(:description)).to eq("New Description")
     end
   end
@@ -42,17 +42,17 @@ RSpec.describe ApplicationHelper do
   describe "#page_robots" do
     it "returns the meta tag with robots directive if robots is set" do
       helper.content_for(:robots, "test")
-      expect(helper.page_robots).to eq('<meta name="robots" content="test">')
+      expect(helper.page_robots_tag).to eq('<meta name="robots" content="test">')
     end
 
     it "returns nil if robots is not set" do
-      expect(helper.page_robots).to be_nil
+      expect(helper.page_robots_tag).to be_nil
     end
   end
 
   describe "#set_page_robots" do
     it "sets the robots directive for the page" do
-      helper.set_page_robots("noindex")
+      helper.page_robots("noindex")
       expect(helper.content_for(:robots)).to eq("noindex")
     end
   end
