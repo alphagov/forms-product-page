fdb13f32d173a60238161530192502ec7115ccf7
Jamie Wilkinson <jamie.wilkinson@digital.cabinet-office.gov.uk>
2024-09-02 16:25:35 +0100

    Add terms of use link to footer

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe "layouts/application", type: :view do
 
     expect(rendered).to have_link("Sign in", href: Settings.forms_admin.base_url)
   end
+
+  describe "the footer" do
+    it "contains a link to the terms of use" do
+      render
+
+      expect(rendered).to have_selector(
+        ".govuk-footer",
+      ) do |footer|
+        expect(footer).to have_link("Terms of use", href: "/terms-of-use")
+      end
+    end
+  end
 end
