901af8b3c9d02176eb9dd378f8f3a2a456fb9bbb
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2025-12-18 13:09:13 +0000

    Add config.hosts configuration to production
    * Add config.hosts configuration to production and a HostPatterns module
    * Add an allowed hosts list based on the `forms.service.gov.uk` domain (there are fewer items on this list because the configuration for `forms-product-page differs slightly from `forms-runner` and `forms-admin`; this list should be sense-checked by another developer)
    * This aligns `forms-product-page` with how config.hosts is configured in `forms-admin` and `forms-runner`

---
+++ lib/host_patterns.rb
@@ -0,0 +1,13 @@
+module HostPatterns
+  DEFAULT_HOST_PATTERNS = [
+    /forms\.service\.gov\.uk/,
+    /www\.forms\.service\.gov\.uk/,
+    /pr-[^.]*\.www\.review\.forms\.service\.gov\.uk/,
+  ].freeze
+
+  def self.allowed_host_patterns
+    additional_patterns = ENV.fetch("ALLOWED_HOST_PATTERNS", "").split(",").map { |pattern| Regexp.new(pattern.strip) }
+
+    [*DEFAULT_HOST_PATTERNS, *additional_patterns]
+  end
+end
