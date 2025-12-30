d4c8531f5513edd88ffff84f8ba8b56bf72795a8
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2025-12-30 11:56:04 +0000

    Skip DNS rebind protection for health check endpoint
    * Ensure host authorization intentionally excludes requests to the health check endpoint
    * The PR is failing because the health check isn't in the list of accepted hosts regex patterns

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,5 +61,5 @@ Rails.application.configure do
   config.hosts = HostPatterns.allowed_host_patterns
 
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
