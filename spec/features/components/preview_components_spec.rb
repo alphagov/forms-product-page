6525e7c92c4ebd12aed2c79348874f0a5604aa2b
Laurence de Bruxelles <laurence.debruxelles@dsit.gov.uk>
2026-03-12 12:09:32 +0200

    Fix component previews
    The preview functionality in the ViewComponent library has changed its
    configuration API since we initially configured it, breaking all of our
    component preview links.
    
    This commit updates to the new configuration, and adds a test so we'll
    be alerted if config changes break the previews again in future.
    
    This commit copies the fix from alphagov/forms-runner#1833.
    
    Co-authored-by: David Biddle <contact@davidbiddle.co.uk>

--- spec/features/components/preview_components_spec.rb
+++ spec/features/components/preview_components_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 describe "Previewing components", type: :system do
+  it "renders component previews" do
+    visit "/preview/"
+
+    expect(page).to have_text("Component")
+  end
+
   it "checks GDS Transport font is available", :js do
     visit "/preview/"
     font_loaded = evaluate_script("document.fonts.check('12px GDS Transport')")
